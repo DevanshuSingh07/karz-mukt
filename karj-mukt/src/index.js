ab15ddaf7c4f80994f8ad632bb50b7a3b68e87a9
devanshu singh <dsdevanshu7@gmail.com>
2025-03-06 19:28:33 +0530

    "vercel speed insight sitemap in public"

--- karj-mukt/src/index.js
+++ karj-mukt/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes,Route } from "react-router";
 import App from "./App";
 import "./index.css"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+  <SpeedInsights/>
   <App/>
   {/* <Routes>
     <Route path="/" element={<HomePage/>}/> 
